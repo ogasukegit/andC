b5adcbc06309e105cae0829a5072a0d0d15d09de
nickbjones <jonesnickb@gmail.com>
2021-01-21 20:34:07 +0900

    product page

--- views/pages/product.php
+++ views/pages/product.php
@@ -1,6 +1,7 @@
 <?php include('../shared/head.php'); ?>
 <div class="ns-product">
   <h1 class="h1">This is the product page!</h1>
+  <p>some content...</p>
 
 </div>
 <?php include('../shared/foot.php'); ?>
