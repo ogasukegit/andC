d46a7b97deaefbcd78fa725fff80851cc4ad3ed7
Antoine Boué <antoineboue1@gmail.com>
2021-01-21 23:05:17 +0900

    home page first draft

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,6 +1,38 @@
 <?php include('../shared/head.php'); ?>
 <div class="ns-home">
-  <h1 class="h1">This is the home page!</h1>
-
+  <div class="section andc-background">
+    <div class="layout">
+      <h2 class="title  txt-center">
+        商品の素材感、美しさを体感してください
+      </h2>
+      <p class="txt-center">
+        ＆C（アンド ・ シー）　は、 視界を程よく遮るスチールメッシュの　『ブラインドメタル』　と、　屋外でも長期間キレイな外観を保つ　『耐候密着コーティング技術』　を生かしたエクステリア製品ブランドとして、2019年秋に誕生しました。
+      </p>
+      <p class="txt-center">
+        私たちが得意とする樹脂コーティング　（Coating）技術を用いて、お客様 (Customer)　の心に寄り添い、心地よい(Comfortable) 空間を演出する商品をご提案してまいります。
+      </p>
+    </div>
+  </div>
+  <div class="section grey-background">
+    <div class="layout">
+      <h2 class="english-title txt-center">
+        PICK UP ITEM
+      </h2>
+      <h2 class="title txt-center">
+        ピックアップアイテム
+      </h2>
+      <div class="pickupitem">
+        <div class="pickupitem flex-child">
+          <img class="pickupitem image" src="/assets/images/home/pickup_item_1.png" alt="pickup_item_1">
+          <img class="pickupitem image" src="/assets/images/home/pickup_item_2.png" alt="pickup_item_2">
+        </div>
+        <div class="pickupitem flex-child">
+          <p class="pickupitem description-head">
+            室外機をオシャレに隠す、お庭のコーディネイトの新提案。
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
 </div>
 <?php include('../shared/foot.php'); ?>
