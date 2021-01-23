b66a38b499031ae2c2458f1c1efc77e1b642bd13
nickbjones <jonesnickb@gmail.com>
2021-01-23 21:01:26 +0900

    product page

--- views/shared/nav.php
+++ views/shared/nav.php
@@ -4,8 +4,8 @@
     "products" => "/products"
   ];
 ?>
+
 <div class="ns-nav">
-  <span>This is the nav</span>
   <?php
     foreach ($links as $text => $link) {
       echo '<a class="nav-item" href="'.$link.'">'.$text.'</a>';
