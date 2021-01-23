525e414ab260e0b4156595ab87e10e3c8e2f1e65
nickbjones <jonesnickb@gmail.com>
2021-01-23 20:59:53 +0900

    navigation

--- views/shared/nav.php
+++ views/shared/nav.php
@@ -1,14 +1,31 @@
 <?php
   $links = [
-    "home" => "/",
-    "products" => "/products"
+    [
+      "text" => "TOP",
+      "url" => "/",
+    ],
+    [
+      "text" => "PRODUCTS",
+      "url" => "/products",
+    ],
+    [
+      "text" => "私たちについて",
+      "url" => "/contact",
+    ],
+    [
+      "text" => "商品注文：お問い合わせ",
+      "url" => "/inquiry",
+    ],
   ];
 ?>
 <div class="ns-nav">
-  <span>This is the nav</span>
-  <?php
-    foreach ($links as $text => $link) {
-      echo '<a class="nav-item" href="'.$link.'">'.$text.'</a>';
-    }
-  ?>
+  <div class="nav layout">
+    <div class="nav-left">
+    </div>
+    <div class="nav-right">
+      <?php foreach ($links as $link): ?>
+        <a class="nav-item" href="<?= $link['url'] ?>"><?= $link['text'] ?></a>
+      <?php endforeach; ?>
+    </div>
+  </div>
 </div>
