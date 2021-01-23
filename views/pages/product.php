6532c9134411d57532a5684ff5c76330afa94ea4
nickbjones <jonesnickb@gmail.com>
2021-01-23 20:22:53 +0900

    product page

--- views/pages/product.php
+++ views/pages/product.php
@@ -1,7 +1,23 @@
+<?php
+  $articles = [
+    [
+      "title" => "Title 1",
+      "description" => "Description 1",
+    ],
+  ];
+?>
+
 <?php include('../shared/head.php'); ?>
 <div class="ns-product">
-  <h1 class="h1">This is the product page!</h1>
-  <p>some content...</p>
+
+  <h1>Product page</h1>
+  <h1>Product page</h1>
+  <?php foreach ($articles as $article): ?>
+    <div class="">
+      <h3><?= $article["title"] ?></h3>
+      <p><?= $article["description"] ?></p>
+    </div>
+  <?php endforeach ?>
 
 </div>
 <?php include('../shared/foot.php'); ?>
