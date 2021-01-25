fc08f1379d327ad5bc1a1db0da9f88a74f3d620a
nickbjones <jonesnickb@gmail.com>
2021-01-25 09:48:34 +0900

    product page

--- views/pages/product.php
+++ views/pages/product.php
@@ -1,8 +1,28 @@
 <?php
-  $articles = [
+  $products = [
     [
-      "title" => "Title 1",
-      "description" => "Description 1",
+      "text" => "室外機カバー",
+      "link" => "/products/product1",
+    ],
+    [
+      "text" => "商品名を追加",
+      "link" => "/products/product2",
+    ],
+    [
+      "text" => "商品名を追加",
+      "link" => "/products/product3",
+    ],
+    [
+      "text" => "商品名を追加",
+      "link" => "/products/product4",
+    ],
+    [
+      "text" => "商品名を追加",
+      "link" => "/products/product5",
+    ],
+    [
+      "text" => "商品名を追加",
+      "link" => "/products/product6",
     ],
   ];
 ?>
@@ -10,14 +30,23 @@
 <?php include('../shared/head.php'); ?>
 <div class="ns-product page">
 
-  <h1>Product page</h1>
-  <h1>Product page</h1>
-  <?php foreach ($articles as $article): ?>
-    <div class="">
-      <h3><?= $article["title"] ?></h3>
-      <p><?= $article["description"] ?></p>
-    </div>
-  <?php endforeach ?>
+  <div class="left">
+    <?php foreach ($products as $product): ?>
+      <div class="">
+        <h3><?= $product["text"] ?></h3>
+        <p><?= $product["link"] ?></p>
+      </div>
+    <?php endforeach ?>
+    <div class="blue-button">商品注文・お問い合わせ</div>
+  </div>
+
+  <div class="right">
+    <h1>Product page</h1>
+    <p>Lor em ip sum dol or sit am et co ns ec te tur, ad ip is ic ing el it. Qu asi, es se ul lam si nt sed, il lo vo lu pt at ib us ut re pu di an dae te mp or ib us ei us pe rf er en dis vo lu pt as. Qu ae di gn is si mos la ud an ti um in ve nt ore ma gni qu am pr ov id ent eos al iq uid.</p>
+    <p>Lor em ip sum dol or sit am et co ns ec te tur, ad ip is ic ing el it. Qu asi, es se ul lam si nt sed, il lo vo lu pt at ib us ut re pu di an dae te mp or ib us ei us pe rf er en dis vo lu pt as. Qu ae di gn is si mos la ud an ti um in ve nt ore ma gni qu am pr ov id ent eos al iq uid.</p>
+    <p>Lor em ip sum dol or sit am et co ns ec te tur, ad ip is ic ing el it. Qu asi, es se ul lam si nt sed, il lo vo lu pt at ib us ut re pu di an dae te mp or ib us ei us pe rf er en dis vo lu pt as. Qu ae di gn is si mos la ud an ti um in ve nt ore ma gni qu am pr ov id ent eos al iq uid.</p>
+    <p>Lor em ip sum dol or sit am et co ns ec te tur, ad ip is ic ing el it. Qu asi, es se ul lam si nt sed, il lo vo lu pt at ib us ut re pu di an dae te mp or ib us ei us pe rf er en dis vo lu pt as. Qu ae di gn is si mos la ud an ti um in ve nt ore ma gni qu am pr ov id ent eos al iq uid.</p>
+  </div>
 
 </div>
 <?php include('../shared/foot.php'); ?>
