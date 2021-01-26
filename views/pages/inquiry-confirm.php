18003d577a8bd248467b8d8c9f4b44b109e54018
nickbjones <jonesnickb@gmail.com>
2021-01-26 21:32:59 +0900

    inquiry

--- views/pages/inquiry-confirm.php
+++ views/pages/inquiry-confirm.php
@@ -1,20 +1,28 @@
 <?php include '../shared/head.php'; ?>
-<div class="ns-inquiry-confirm page">
-  <h1 class="h1">業務用（法人の方）<br/>ご注文・お問い合わせ</h1>
-  <?php $breadcrumb = [
-    [
-      "link" => "/inquiry",
-      "text" => "業務用（法人の方）ご注文・お問い合わせ",
-    ],
-    [
-      "link" => false,
-      "text" => "入力内容確認",
-    ],
-  ]; ?>
-  <?php include '../shared/breadcrumb.php'; ?>
+<div class="ns-inquiry page">
+  <div class="layout">
+    <h1 class="h1">業務用（法人の方）<br/>ご注文・お問い合わせ</h1>
+    <?php
+      $breadcrumb = [
+        [
+          "link" => "/inquiry",
+          "text" => "業務用（法人の方）ご注文・お問い合わせ",
+        ],
+        [
+          "link" => false,
+          "text" => "入力内容確認",
+        ],
+      ]; 
+    ?>
+    <?php include '../shared/breadcrumb.php'; ?>
+    <div class="form-status">
+      <div class="form-status__step">ステップ1<br>入力</div>
+      <div class="form-status__step form-status__step--active">ステップ2<br>入力内容確認</div>
+      <div class="form-status__step">ステップ3<br>送信完了</div>
+    </div>
+    <h2 class="h2">入力内容確認</h2>
 
-  <div class="current-status">current-status</div>
-
-  <a href="inquiry-complete.php">confirm</a>
+    <a href="inquiry-complete.php">confirm</a>
+  </div>
 </div>
 <?php include '../shared/foot.php'; ?>
--- views/pages/inquiry-input.php
+++ views/pages/inquiry-input.php
@@ -1,16 +1,24 @@
 <?php include '../shared/head.php'; ?>
-<div class="ns-inquiry-input page">
-  <h1 class="h1">業務用（法人の方）<br/>ご注文・お問い合わせ</h1>
-  <?php $breadcrumb = [
-    [
-      "link" => false,
-      "text" => "業務用（法人の方）ご注文・お問い合わせ",
-    ],
-  ]; ?>
-  <?php include '../shared/breadcrumb.php'; ?>
+<div class="ns-inquiry page">
+  <div class="layout">
+    <h1 class="h1">業務用（法人の方）<br/>ご注文・お問い合わせ</h1>
+    <?php
+      $breadcrumb = [
+        [
+          "link" => false,
+          "text" => "業務用（法人の方）ご注文・お問い合わせ",
+        ],
+      ];
+    ?>
+    <?php include '../shared/breadcrumb.php'; ?>
+    <div class="form-status">
+      <div class="form-status__step form-status__step--active">ステップ1<br>入力</div>
+      <div class="form-status__step">ステップ2<br>入力内容確認</div>
+      <div class="form-status__step">ステップ3<br>送信完了</div>
+    </div>
+    <h2 class="h2">入力内容</h2>
 
-  <div class="current-status">current-status</div>
-
-  <a href="inquiry-confirm.php">next</a>
+    <a href="inquiry-confirm.php">next</a>
+  </div>
 </div>
 <?php include '../shared/foot.php'; ?>
