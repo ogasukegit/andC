3c3bb870319f85f7b80f9477056519051586b7fd
nickbjones <jonesnickb@gmail.com>
2021-01-27 11:26:04 +0900

    inquiry

--- views/pages/inquiry-confirm.php
+++ views/pages/inquiry-confirm.php
@@ -11,29 +11,94 @@
       "text" => "入力内容確認",
     ],
   ];
+
+  $inquiryType = $_POST["inquiry-type"];
+  $productName = $_POST["product-name"];
+  $nameKanji = $_POST["name-kanji"];
+  $nameKana = $_POST["name-kana"];
+  $organizationName = $_POST["organization-name"];
+  $departmentName = $_POST["department-name"];
+  $postalCode1 = $_POST["postal-code-1"];
+  $postalCode2 = $_POST["postal-code-2"];
+  $address = $_POST["address"];
+  $phone = $_POST["phone"];
+  $email = $_POST["email"];
+  $fax = $_POST["fax"];
+  $message = $_POST["message"];
 ?>
 <?php include './_inquiry-head.php'; ?>
   <div class="layout">
     <h2 class="inquiry-h2">入力内容確認</h2>
-    <p>下記内容でお間違いありませんか？</p>
 
-    <hr>
-    <h1>CONFIRM</h1>
-    <hr>
+    <!-- change post destination (handle send and redirect to /inquiry/complete) -->
+    <form class="form" method="post" action="inquiry-confirm.php">
+      <p class="form__top-text">下記内容でお間違いありませんか？</p>
+      <div class="form__row">
+        <div class="form__row-left">お問い合わせ項目</div>
+        <div class="form__row-right form__row-output"><?= $inquiryType ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">商品名</div>
+        <div class="form__row-right form__row-output"><?= $productName ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">氏名</div>
+        <div class="form__row-right form__row-output"><?= $nameKanji ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">氏名 よみがな</div>
+        <div class="form__row-right form__row-output"><?= $nameKana ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">会社or団体名</div>
+        <div class="form__row-right form__row-output"><?= $organizationName ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">部署名</div>
+        <div class="form__row-right form__row-output"><?= $departmentName ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">郵便番号</div>
+        <div class="form__row-right form__row-output"><?= $postalCode1 ?>-<?= $postalCode2 ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">住所</div>
+        <div class="form__row-right form__row-output"><?= $address ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">お電話番号</div>
+        <div class="form__row-right form__row-output"><?= $phone ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">メールアドレス</div>
+        <div class="form__row-right form__row-output"><?= $email ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">FAX</div>
+        <div class="form__row-right form__row-output"><?= $fax ?></div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">お問い合わせ内容</div>
+        <div class="form__row-right form__row-output"><?= $message ?></div>
+      </div>
+      <h2 class="inquiry-h2">個人情報の取り扱い</h2>
+      <p>個人情報の取り扱いについて、詳しくは弊社の<a href="https://www.imcjpn.co.jp/policy/">プライバシーポリシー</a>をご覧ください。</p>
+      <div class="form__row">
+        <div class="form__row-left">個人情報の取り扱い</div>
+        <div class="form__row-right">
+          <label class="clickable"><input type="checkbox" id="personal-info" required>&nbsp;同意します</label>
+        </div>
+      </div>
+      <div class="button-wrapper">
+        <input class="button button--blue" type="submit" value="修正する">
+        <a class="button button--black-ghost" href="inquiry-input.php">送信</a>
+      </div>
+    </form>
 
-    <h2 class="inquiry-h2">個人情報の取り扱い</h2>
-    <p>個人情報の取り扱いについて、詳しくは弊社のプライバシーポリシーをご覧ください。</p>
-
-    個人情報の取り扱い
-
-    □同意します
-
-    <div class="button-wrapper">
-      <a class="button button--blue" href="inquiry-input.php">送信</a>
-      <a class="button button--black-ghost" href="inquiry-complete.php">修正する</a>
-    </div>
   </div>
-  <div class="go-to-first-view__wrapper">
-    <a class="go-to-first-view__button" href="inquiry-input.php">ページ上部へ</a>
-  </div>
+
+  <!-- <div class="go-to-first-view__wrapper"> -->
+    <!-- change link to "/inquiry" -->
+    <!-- <a class="go-to-first-view__button" href="inquiry-input.php">ページ上部へ</a> -->
+  <!-- </div> -->
 <?php include './_inquiry-foot.php'; ?>
--- views/pages/inquiry-input.php
+++ views/pages/inquiry-input.php
@@ -11,51 +11,103 @@
 <?php include './_inquiry-head.php'; ?>
   <div class="layout">
     <h2 class="inquiry-h2">入力内容</h2>
-    <p><span class="required"></span>は必須項目となります。必ずご記入ください。</p>
 
-    <hr>
-    <h1>FORM</h1>
-    <hr>
+    <!-- change post destination to "/inquiry/confirm" -->
+    <form class="form" method="post" action="inquiry-confirm.php">
+      <p class="form__top-text"><span class="required"></span>は必須項目となります。必ずご記入ください。</p>
+      <div class="form__row">
+        <div class="form__row-left">お問い合わせ項目</div>
+        <div class="form__row-right">
+          <label class="clickable"><input type="radio" name="inquiry-type" value="お問い合わせ">お問い合わせ</label>
+          <span class="spacer"></span>
+          <label class="clickable"><input type="radio" name="inquiry-type" value="その他">その他</label>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">商品名</div>
+        <div class="form__row-right">
+          <input type="text" name="product-name" class="form__text-input">
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">氏名</div>
+        <div class="form__row-right">
+          <input type="text" name="name-kanji" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">氏名 よみがな</div>
+        <div class="form__row-right">
+          <input type="text" name="name-kana" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">会社or団体名</div>
+        <div class="form__row-right">
+          <input type="text" name="organization-name" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">部署名</div>
+        <div class="form__row-right">
+          <input type="text" name="department-name" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">郵便番号</div>
+        <div class="form__row-right">
+          <input type="number" name="postal-code-1" minlength="3" maxlength="3" class="form__text-input form__text-input--postal" required>
+          <span class="spacer">-</span>
+          <input type="number" name="postal-code-2" minlength="4" maxlength="4" class="form__text-input form__text-input--postal" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">住所</div>
+        <div class="form__row-right">
+          <input type="text" name="address" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">お電話番号</div>
+        <div class="form__row-right">
+          <input type="tel" name="phone" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left required">メールアドレス</div>
+        <div class="form__row-right">
+          <input type="email" name="email" class="form__text-input" required>
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">FAX</div>
+        <div class="form__row-right">
+          <input type="tel" name="fax" class="form__text-input">
+        </div>
+      </div>
+      <div class="form__row">
+        <div class="form__row-left">お問い合わせ内容</div>
+        <div class="form__row-right">
+          <textarea name="message" cols="30" rows="10" class="form__text-input"></textarea>
+        </div>
+      </div>
+      <h2 class="inquiry-h2">個人情報の取り扱い</h2>
+      <p>個人情報の取り扱いについて、詳しくは弊社の<a href="https://www.imcjpn.co.jp/policy/">プライバシーポリシー</a>をご覧ください。</p>
+      <div class="form__row">
+        <div class="form__row-left">個人情報の取り扱い</div>
+        <div class="form__row-right">
+          <label class="clickable"><input type="checkbox" id="personal-info" required>&nbsp;同意します</label>
+        </div>
+      </div>
+      <div class="button-wrapper">
+        <input class="button button--blue" type="submit" value="入力内容確認">
+      </div>
+    </form>
 
-    <h2 class="inquiry-h2">個人情報の取り扱い</h2>
-    <p>個人情報の取り扱いについて、詳しくは弊社のプライバシーポリシーをご覧ください。</p>
-
-    個人情報の取り扱い
-
-    □同意します
-
-    <div class="button-wrapper">
-      <a class="button button--blue" href="inquiry-confirm.php">入力内容確認</a>
-    </div>
   </div>
-  <div class="go-to-first-view__wrapper">
-    <a class="go-to-first-view__button" href="inquiry-input.php">ページ上部へ</a>
-  </div>
+
+  <!-- <div class="go-to-first-view__wrapper"> -->
+    <!-- change link to "/inquiry" -->
+    <!-- <a class="go-to-first-view__button" href="inquiry-input.php">ページ上部へ</a> -->
+  <!-- </div> -->
 <?php include './_inquiry-foot.php'; ?>
-
-
-お問い合わせ項目
-
-〇お問い合わせ　〇その他
-
-商品名
-
-氏名※
-　
-氏名 よみがな※
-
-会社or団体名※
-
-部署名※
-
-郵便番号※
-
-住所※
-
-お電話番号※
-
-メールアドレス※
-
-FAX
-
-お問い合わせ内容
