338c46b7789ed6d2d4f12a094260ebf121d02686
Antoine Boué <antoineboue1@gmail.com>
2021-01-28 23:56:08 +0900

    Product page - progress

--- views/pages/product.php
+++ views/pages/product.php
@@ -1,4 +1,5 @@
 <?php
+$pageName = "product";
 $metaData = [
   "title" => "シンプルで長く使える室外機カバー（白・茶）｜業務用・一般用｜＆C－ブランドサイト",
   "description" => "サビ・劣化に強く、丈夫なエアコン室外機カバーです。積雪地域や日除け、省エネ効果に最適です。素材は、スチール製で、色は白（ホワイト）とブラウンの2種類ご用意。サイズは、標準サイズと大型室外機用をご用意しています。",
@@ -41,69 +42,237 @@ $products = [
   ],
 ];
 $slide_images = [
-  "/product/top1.png",
-  // "/product/top2.png",
-  // "/product/top3.png",
-  // "/product/top4.png",
-  // "/product/top5.png",
-  // "/product/top6.png",
+  "/product/oshare1.png",
+  "/product/oshare2.png",
+  "/product/oshare3.png",
+  "/product/oshare4.png",
+  "/product/oshare1.png",
+  "/product/oshare2.png",
+];
+$features = [
+  [
+    "title" => "耐候密着コーティングを採用",
+    "description" => "丈夫なスチール製品も、　風雨にさらされる屋外ではサビや腐食の可能性があります。<br><br>
+      そこで対候性に優れたポリエチレンをたっぷりとコーティングすることで、 長期間キレイにお使いいただけるとともに、 柔らかな質感を与え、直射日光による本体の過熱や照り返しを防ぎます。",
+    "image" => [
+      [
+        "src" => "/product/list1.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "室外機を隠しながら守る正面ネット",
+    "description" => "生活感の出る室外機をそれとなく目隠しする正面ネットは、 小さな屋根がたくさん連なった形をしています。<br><br>
+      人目や直射日光、雨、雪、 砂埃から室外機を隠しながらも、 室外機の廃棄を妨げないので、 カバーをしたままでも冷暖房の効果を阻害しにくくなっています。",
+    "image" => [
+      [
+        "src" => "/product/list2.png",
+      ],
+      [
+        "src" => "/product/list3.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "カバーのトップは、　耐荷重10kgの強度",
+    "description" => "ベランダや庭で、 観葉植物や植木鉢、 季節のオーナメントを飾るなど、天板を有効活用いただけます。",
+    "image" => [
+      [
+        "src" => "/product/list4.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "サイズ展開",
+    "description" => "どのメーカーの、  どのサイズの室外機もカバーできる2種展開。<br><br>
+      おおよそ6～12畳向けの室外機をカバーできる標準サイズと、 12畳以上の大型室外機用の大サイズをご用意しています。<br><br>
+      （適応サイズは2020年夏時点の情報です）
+    ",
+    "image" => [
+      [
+        "src" => "/product/list5.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "カラー展開",
+    "description" => "家の外壁やお庭と調和させやすいカラー展開。<br><br>
+      アイボリー調の柔らかなホワイト （白色） と、 落ち着いた色合いのブラウン（茶色）をご用意しています。
+    ",
+    "image" => [
+      [
+        "src" => "/product/list6.png",
+        "caption" => "ホワイト",
+      ],
+      [
+        "src" => "/product/list7.png",
+        "caption" => "ブラウン",
+      ]
+    ]
+  ],
+  [
+    "title" => "女性おひとりでも組み立て可能",
+    "description" => "プラスドライバーをご用意していただき、付属ねじで固定するだけなので、簡単に組み立てられます。<br><br>
+      重さ10kgなので女性でも設置いただけます。
+    ",
+    "image" => [
+      [
+        "src" => "/product/list8.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "アジャスターで高さ調節可能",
+    "description" => "設置場所の状態に合わせてアジャスターで高さを調整。 がたつきを抑え、　安定して設置できます。",
+    "image" => [
+      [
+        "src" => "/product/list9.png",
+      ]
+    ]
+  ],
+  [
+    "title" => "結束バンドで本体と固定",
+    "description" => "付属の結束バンドで室外機本体や周辺の配管等とカバーをつなぎ固定すれば、　台風などの災害時にも安心です。　必要に応じてお使いください。",
+    "image" => [
+      [
+        "src" => "/product/list10.png",
+      ]
+    ]
+  ],
+
 ]
 ?>
 
 <?php include('../shared/head.php'); ?>
 <div class="ns-product page">
-  <div class="nav">
-    <p class="nav__title">
-      PRODUCTS
-    </p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-    <p class="nav__item">―室外機カバー</p>
-  </div>
-  <div class="layout product-main">
-    <?php include '../shared/breadcrumb.php'; ?>
-    <h2 class="product-title">
-      樹脂コーティング会社が本気で作った、 サビ ・ 劣化に強い、 シンプルデザインのエアコン室外機カバー 【ホワイト ・ ブラウン】 【スチール製】
-    </h2>
-    <p class="product-description">
-      場になじむ、 シンプルモダンなデザイン。 細部までこだわり長く使えるエアコン室外機カバーです。
-    </p>
-    <div class="product-slider">
-      <div class="product-slider__button">
-        <div class="product-slider__icon product-slider__icon--left"></div>
+  <div class="product-layout">
+    <div class="nav hide-sp">
+      <p class="nav__title">
+        PRODUCTS
+      </p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+      <p class="nav__item">―室外機カバー</p>
+    </div>
+    <div class="layout product-main">
+      <?php include '../shared/breadcrumb.php'; ?>
+      <h2 class="product-title">
+        樹脂コーティング会社が本気で作った、 サビ ・ 劣化に強い、 シンプルデザインのエアコン室外機カバー 【ホワイト ・ ブラウン】 【スチール製】
+      </h2>
+      <p class="product-description">
+        場になじむ、 シンプルモダンなデザイン。 細部までこだわり長く使えるエアコン室外機カバーです。
+      </p>
+      <div class="product-slider">
+        <div class="product-slider__button" onClick="previousSlide()">
+          <div class="product-slider__icon product-slider__icon--left"></div>
+        </div>
+        <div class="product-slider__image-container">
+          <?php foreach ($slide_images as $image) : ?>
+            <img class="product-slider__image" src=<?= $imgPath . $image ?>>
+          <?php endforeach ?>
+        </div>
+        <div class="product-slider__button" onClick="nextSlide()">
+          <div class="product-slider__icon product-slider__icon--right"></div>
+        </div>
       </div>
-      <div class="product-slider__image-container" style="background-image: url(<?= $imgPath . $slide["Image"] ?>)">
-        <?php foreach ($slide_images as $image) : ?>
-          <img src=<?= $imgPath . $image ?>>
+      <div class="sub-slides">
+        <?php foreach ($slide_images as $x => $image) : ?>
+          <div class="sub-slide" onClick="changeSlide(<?= $x ?>)">
+            <img class="sub-slide__img" src=<?= $imgPath . $image ?>>
+          </div>
         <?php endforeach ?>
       </div>
-      <div class="product-slider__button">
-        <div class="product-slider__icon product-slider__icon--right"></div>
+      <div class="product-buttons">
+        <a class="arrow-button" href="">
+          一般のお客様<br>
+          お問い合わせ
+        </a>
+        <a class="arrow-button" href="">
+          法人のお客様<br>
+          お問い合わせ
+        </a>
       </div>
-    </div>
-    <div class="product-slider">
-      <div class="product-slider__button">
-        <div class="product-slider__icon product-slider__icon--left"></div>
+      <div class="fashion">
+        <h2 class="middle__title txt-center">
+          おしゃれに隠し、 長く使う
+        </h2>
+        <div class="fashion-block">
+          <div class="fashion-block__img-container">
+            <img class="fashion-block__img" src=<?= $imgPath . "/product/oshare1.png" ?>>
+          </div>
+          <div class="fashion-block__img-container">
+            <img class="fashion-block__img" src=<?= $imgPath . "/product/oshare2.png" ?>>
+          </div>
+          <div class="fashion-block__img-container">
+            <img class="fashion-block__img" src=<?= $imgPath . "/product/oshare3.png" ?>>
+          </div>
+          <div class="fashion-block__img-container">
+            <img class="fashion-block__img" src=<?= $imgPath . "/product/oshare4.png" ?>>
+          </div>
+        </div>
       </div>
-      <div class="product-slider__image-container" style="background-image: url(<?= $imgPath . $slide["Image"] ?>)">
-        <?php foreach ($slide_images as $image) : ?>
-          <img src="http://placehold.it/900x450">
+      <div class="features">
+        <h2 class="middle__title txt-center">
+          頑丈でサビや腐食に強い、室外機カバー
+        </h2>
+        <?php foreach ($features as $feature) : ?>
+          <div class="feature">
+            <div class="feature__images">
+              <?php foreach ($feature["image"] as $image) : ?>
+                <div class="feature__image-block">
+                  <img class="feature__image" src=<?= $imgPath . $image["src"] ?>>
+                  <p class="feature__description"><?= $image["caption"] ?></p>
+                </div>
+              <?php endforeach ?>
+            </div>
+            <div class="feature__text">
+              <h3 class="feature__title"><?= $feature["title"] ?></h3>
+              <p class="feature__description"><?= $feature["description"] ?></p>
+            </div>
+          </div>
         <?php endforeach ?>
-      </div>
-      <div class="product-slider__button">
-        <div class="product-slider__icon product-slider__icon--right"></div>
       </div>
     </div>
   </div>
-
 </div>
 
 <script>
+  const slides = document.getElementsByClassName("product-slider__image");
 
+  let currentIndex = 0;
+
+  changeSlide(currentIndex);
+
+  function changeSlide(x) {
+    currentIndex = x;
+    Array.from(slides).forEach((slide, index) => {
+      slide.style.display = index === x ? "block" : "none";
+    })
+  }
+
+  function nextSlide() {
+    currentIndex += 1;
+    if (currentIndex === slides.length) {
+      currentIndex = 0;
+    }
+    Array.from(slides).forEach((slide, index) => {
+      slide.style.display = index === currentIndex ? "block" : "none";
+    })
+  }
+
+  function previousSlide() {
+    currentIndex -= 1;
+    if (currentIndex === -1) {
+      currentIndex = slides.length - 1;
+    }
+    Array.from(slides).forEach((slide, index) => {
+      slide.style.display = index === currentIndex ? "block" : "none";
+    })
+  }
 </script>
 <?php include('../shared/foot.php'); ?>
