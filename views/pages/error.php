7a8792ab48b6b5be53a8b72351c9a2ef5b0698ed
nickbjones <jonesnickb@gmail.com>
2021-02-21 21:11:30 +0900

    adding error page

---
+++ views/pages/error.php
@@ -0,0 +1,19 @@
+<?php
+  // required page data
+  $pageName = "home";
+  $metaData = [
+    "title" => "＆C｜ブランドサイト―優れた樹脂コーティングをエクステリアに。機能的で美しく。",
+    "description" => "樹脂コーティングのパイオニアとして、金属と樹脂の技術を融合・応用した商品をお客様にお届けいたします。商品の素材感・美しさをぜひ体感してください。",
+    "keywords" => "",
+  ];
+?>
+
+<?php include('../shared/head.php'); ?>
+<div class="ns-error page">
+  <?php include('../shared/navigation.php'); ?>
+  <div class="layout">
+    <h2 class="txt-center">Page does not exist</h2>
+    <p class="txt-center"><a href="/">Go home</a></p>
+  </div>
+</div>
+<?php include('../shared/foot.php'); ?>
