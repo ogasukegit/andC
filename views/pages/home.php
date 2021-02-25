15d969d35b5c521100cc0e7a12712e7a0d50be54
nickbjones <jonesnickb@gmail.com>
2021-02-25 21:37:34 +0900

    moving content to content dir

--- views/pages/home.php
+++ views/pages/home.php
@@ -7,24 +7,7 @@
     "keywords" => "",
   ];
 
-  // temporary data
-  $slides = [
-    "/home/top_image_1.jpg",
-    "/home/top_image_2.png"
-  ];
-  $lineup_articles = [
-    [
-      "Image" => "/home/lineup_1.png",
-      "Title" => "エアコン室外機カバー<br>（ホワイト・ブラウン）",
-      "Description" => 'サビ・腐食に強く、キレイな状態が長持ちします。お庭に調和しながら、室外機を人目から隠します。'
-    ],
-  ];
-  $news_list = [
-    [
-      "Date" => "2021.3.26",
-      "Content" => "&Cオフィシャルサイトを開設しました。"
-    ],
-  ];
+  include('../../content/home.php');
 ?>
 
 <?php include('../shared/head.php'); ?>
